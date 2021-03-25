7bd59191ff2b3872684145163e9bcb7697e3d89c
Taranpreet Singh Chabbra <singhh3010@gmail.com>
2021-03-25 10:34:38 +0530

    updated

--- demo/src/main/java/com/example/demo/controllers/PersonController.java
+++ demo/src/main/java/com/example/demo/controllers/PersonController.java
@@ -1,4 +1,20 @@
 package com.example.demo.controllers;
 
+import com.example.demo.DAO.Person;
+import com.example.demo.DBManager.DBOperations;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@RequestMapping
 public class PersonController {
+
+    @RequestMapping(value = "/getPerson", method = RequestMethod.GET)
+    public List<Person> getPersons() {
+
+        //calling dao to get the objects from db
+        return DBOperations.getPersons();
+    }
 }
