b1590395ba1c64e30dfed765dfd1cf6e60fb87ca
Taranpreet Singh Chabbra <singhh3010@gmail.com>
2021-04-01 19:24:41 +0530

    updated_ports_connection

--- demo/src/main/java/com/example/demo/DBManager/DBOperations.java
+++ demo/src/main/java/com/example/demo/DBManager/DBOperations.java
@@ -3,6 +3,8 @@ package com.example.demo.DBManager;
 import com.example.demo.DAO.Person;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.List;
 
 public class DBOperations {
@@ -10,8 +12,17 @@ public class DBOperations {
     private static volatile Connection connection;  //if thread t1 ->open so that no other thread should made open connection (singleton pattern) ->volatile
 
 
-    public static Connection getConnection(){
-    return null;
+    public static Connection getConnection() throws SQLException {
+            if(connection!=null)
+            {
+                synchronized (DBOperations.class){
+                    if(connection!=null)
+                    {
+                        connection= DriverManager.getConnection("jdbc:mysql:127.0.0.1:/3306");
+                    }
+                }
+            }
+        return null;
     }
 
     public static void CloseConnection() {
