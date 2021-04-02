c20dc427898d895cfd10bf0a08be52469490c158
Taranpreet Singh Chabbra <singhh3010@gmail.com>
2021-04-02 15:32:14 +0530

    added insert_method(POST)

--- demo/src/main/java/com/example/demo/controllers/PersonController.java
+++ demo/src/main/java/com/example/demo/controllers/PersonController.java
@@ -2,10 +2,13 @@ package com.example.demo.controllers;
 
 import com.example.demo.DAO.Person;
 import com.example.demo.DBManager.DBOperations;
+import com.example.demo.request.CreateRequest;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.sql.SQLException;
 import java.util.List;
 
 @RequestMapping
@@ -16,5 +19,11 @@ public class PersonController {
 
         //calling dao to get the objects from db
         return DBOperations.getPersons();
+    }
+
+
+    @RequestMapping(value = "/insertPerson",method = RequestMethod.POST)
+    public void insertPerson(@RequestBody CreateRequest request) throws SQLException {
+       DBOperations.insertPerson(request);
     }
 }
