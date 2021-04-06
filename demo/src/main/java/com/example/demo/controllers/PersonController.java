1548f9e3cec2755ead6a72c562cf9d91410e466f
Taranpreet Singh Chabbra <singhh3010@gmail.com>
2021-04-06 19:35:24 +0530

    Create PersonController.java

--- demo/src/main/java/com/example/demo/controllers/PersonController.java
+++ demo/src/main/java/com/example/demo/controllers/PersonController.java
@@ -3,10 +3,7 @@ package com.example.demo.controllers;
 import com.example.demo.DAO.Person;
 import com.example.demo.DBManager.DBOperations;
 import com.example.demo.request.CreateRequest;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -14,16 +11,21 @@ import java.util.List;
 @RequestMapping
 public class PersonController {
 
-    @RequestMapping(value = "/getPerson", method = RequestMethod.GET)
-    public List<Person> getPersons() {
+    @GetMapping("/getPersons")
+    public List<Person> getPersons() throws SQLException {
 
         //calling dao to get the objects from db
         return DBOperations.getPersons();
     }
 
+    @PostMapping("/createTable")
+    public void createTable(@RequestParam(value = "name") String name) throws SQLException {
+        DBOperations.createTable(name);
+    }
 
-    @RequestMapping(value = "/insertPerson",method = RequestMethod.POST)
+    @PostMapping("/insertPerson")
     public void insertPerson(@RequestBody CreateRequest request) throws SQLException {
-       DBOperations.insertPerson(request);
+
+        DBOperations.insertPerson(request);
     }
 }
